a8eff62ddb3837ebde3b4329bf61f21cb31766bc
Joan Alvarez <joanquique@gmail.com>
2025-06-29 21:44:10 -0600

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+//     { path: 'goles', component: GolesComponent },
+//   { path: 'asistencias', component: AsistenciasComponent },
+//   { path: 'puntos', component: PuntosComponent },
+  { path: '', redirectTo: '/goles', pathMatch: 'full' }
+];
